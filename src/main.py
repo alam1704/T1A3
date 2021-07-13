42986cbe13999bfcbe4f488b3d764eafc8f0a0c8
alexlam <alexlam1994@live.com>
2021-07-13 22:23:22 +1000

    added art 5.2, created clear function, created exit_game function and welcome screen function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,36 @@
-import emoji
 import os
+import random
+import time
+import sys
 
-os.system("clear")
-print(emoji.emojize("Python is :thumbs_up:"))
+import art
+import emoji
+import cowsay
+
+#clear screen function
+def clear():
+    if os.name == "nt":
+        command  = "cls"
+    else:
+        command = "clear"
+    
+    os.system(command)
+
+#function for exiting the game.
+def exit_game():
+    clear()
+    print("\n")
+    print("Thanks for playing")
+    print("\n")
+    time.sleep(3)
+    clear()
+    exit()
+
+#welcome screen
+def welcome():
+    #to add the pyramid art soon... not sure if i want to add a separate function or not.
+    print(art.text2art("""Who  wants 
+    to  build 
+    a  pyramid? """, font="pyramid"))
+    
+
